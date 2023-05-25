34363549dc1e009304ec7bc7734291ddd5422269
Ivan <whitestranger7@gmail.com>
2023-05-25 20:59:09 +0200

    dd

--- scenes/game_objects/enemy/enemy.gd
+++ scenes/game_objects/enemy/enemy.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
+var health_component: HealthComponent
+var drop_component: DropComponent
+
 
 const SPEED = 40
 
 
 func _ready() -> void:
+	health_component = $HealthComponent
+	drop_component = $DropComponent
 	$Area2D.area_entered.connect(on_area_entered)
+	health_component.on_died.connect(on_died)
 
 
 func _process(_delta: float) -> void:
@@ -21,5 +27,9 @@ func get_player_direction() -> Vector2:
 	return Vector2.ZERO
 
 
-func on_area_entered(area: Area2D) -> void:
-	queue_free()
+func on_area_entered(_area: Area2D) -> void:
+	health_component.take_damage(100)
+
+
+func on_died() -> void:
+	drop_component.drop_item()
